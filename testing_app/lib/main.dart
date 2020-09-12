10b81b4d16a2ec1b41744fe6a080fa411b51317a
nowxel <58902795+nowxel@users.noreply.github.com>
2020-09-12 16:01:29 +0300

    Add files via upload

--- testing_app/lib/main.dart
+++ testing_app/lib/main.dart
@@ -33,12 +33,6 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-
-class MyHomePage extends StatelessWidget {
-  final List<Friend> friends = [
-    Friend(name: "Mary", email)
-  ]
-}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -138,10 +132,10 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+            ), //Text
+          ], //Widget
+        ), //Column
+      ), //Center
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
