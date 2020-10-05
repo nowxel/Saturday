6588eb4b708c0cf8076decaafbf6f25ed3deb7e1
nowxel <58902795+nowxel@users.noreply.github.com>
2020-10-06 02:21:50 +0300

    Add files via upload

--- testing_app/lib/main.dart
+++ testing_app/lib/main.dart
@@ -1,76 +1,181 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:hello_providers/models/friend.dart';
-import 'package:hello_providers/screens/screen_invited.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-  ));
-
-class MyApp extends StatelessWidget {
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(PlatformView());
 }
 
-class _MyAppState extends State<MyApp> {
-  
-  List colors = [Colors.red, Colors.green, Colors.yellow];
-  Random random = new Random();
-
-  int index = 0;
-
-  void changeIndex() {
-    setState(() => index = random.nextInt(3));
-  }
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () => changeIndex (),
-        child: Text(''),
-        color: colors[index],
-      )
-    )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('Let' + '`' + 's play a game'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PlatformView()),
+              );
+            }),
+      ),
+    );
   }
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Second Route"),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+            )),
+        body: Center(
+            child: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(20),
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.red, Colors.blue],
+                begin: FractionalOffset.centerLeft,
+                end: FractionalOffset.centerRight,
+              ),
+            ),
+            child: FlatButton(
+              child: Text(
+                '100%',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstRoute()),
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.lightGreen, Colors.orange],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+              ),
+            ),
+            child: FlatButton(
+              child: Text(
+                '70%',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstRoute()),
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.lightGreen, Colors.orange],
+                begin: FractionalOffset.topRight,
+                end: FractionalOffset.bottomLeft,
+              ),
+            ),
+            child: FlatButton(
+              child: Text(
+                '50%',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdRoute()),
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            width: 200,
+            height: 50,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black45, Colors.blue, Colors.blueGrey],
+                begin: FractionalOffset.center,
+                end: FractionalOffset.bottomRight,
+              ),
+            ),
+            child: FlatButton(
+              child: Text(
+                '40%',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdRoute()),
+                );
+              },
+            ),
+          ),
+        ])));
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Third Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+          child: Text('Mazel Tov!'),
+        ),
+      ),
+    );
+  }
+}
+
+class PlatformView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sclaware',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.grey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Sclaware'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -79,68 +184,155 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const MethodChannel _methodChannel =
+      MethodChannel('samples.flutter.io/platform_view');
+
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hey there',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ), //Text
-          ], //Widget
-        ), //Column
-      ), //Center
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  Future<void> _launchPlatformCount() async {
+    final int platformCounter =
+        await _methodChannel.invokeMethod('switchView', _counter);
+    setState(() {
+      _counter = platformCounter;
+    });
   }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+            title: Text(widget.title),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              //onPressed:() => Navigator.pop(context, false),
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => FirstRoute())),
+            )),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: double.infinity,
+              //color: Colors.purple,
+              alignment: Alignment.center,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black, width: 3),
+              ),
+              child: Text("Hello! Let's play a game",
+                  style: TextStyle(fontSize: 20)),
+            ),
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: [
+                  Expanded(
+                    /*1*/
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /*2*/
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Text(
+                            'Invitation',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Priglachennie',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  /*3*/
+                  Icon(
+                    Icons.volume_up,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(32),
+              child: Row(
+                children: [
+                  Expanded(
+                    /*1*/
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        /*2*/
+                        Container(
+                          padding: const EdgeInsets.only(bottom: 8),
+                          child: Text(
+                            'Invitation',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          'Priglachennie',
+                          style: TextStyle(
+                            color: Colors.grey[500],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  /*3*/
+                  Icon(
+                    Icons.volume_up,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.transparent,
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              child: FlatButton(
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SecondRoute(),
+                      ));
+                },
+                color: Colors.purple,
+                child: Text(
+                  "Nachut virock",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Raleway',
+                    fontSize: 22.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      );
 }
